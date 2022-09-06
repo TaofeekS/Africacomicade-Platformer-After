b38637518039818d72bedfd0c57d769ab8aa27c3
TaofeekS <taofeekonaniyi@gmail.com>
2022-09-06 15:12:14 +0100

    BackgroundMovement

---
+++ AfricacomicadeGame/src/Script/backgroundImages.gd
@@ -0,0 +1,20 @@
+extends HBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _physics_process(delta):
+	rect_position.y += 20 *delta
+	
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	get_parent().move_child(self,0)
+	rect_position = Vector2(0,-64)
